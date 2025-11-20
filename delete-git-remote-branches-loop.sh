5229e897f0d908436c55600c2257c7865113c230
Zo718 <30537808+zo718@users.noreply.github.com>
2025-11-20 08:39:16 -0500

    Update delete-git-remote-branches-loop.sh

--- delete-git-remote-branches-loop.sh
+++ delete-git-remote-branches-loop.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #version 1.0 - Written by Alfredo Jo ® - 2025
-#Basic script that iterates over all remote Git branches and removes them. Verified on macOS.
+#Basic script that iterates over all remote Git branches in my laptop and removes them. Verified on macOS.
 
 for i in `git branch -r|grep -v main`; do git branch -rd $i; done
